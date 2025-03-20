e714cb77a0e94e13a03322aa88293c2c7d337e50
Animesh Gupta <117362788+anxmeshhh@users.noreply.github.com>
2025-03-20 20:15:41 +0530

    Update settings.py

--- hfs/settings.py
+++ hfs/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-qpp#p3eyndv5x9)h&giuk=91c4gzd7rr71sfie!l@oe^tz!)f)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['med-production-d19d.up.railway.app']
+
 
 
 # Application definition
